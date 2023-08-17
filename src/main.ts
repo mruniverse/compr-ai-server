cdccf99bd25abdd7e2d6bd2f0e6d991a5a702001
mruniverse <yurilucaspg@gmail.com>
2023-08-17 18:20:30 -0300

    New cors config

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: 'http://dev.a3recovery.com.br',
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
