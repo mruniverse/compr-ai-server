985b6ccb02f8bb47b3bb4b51d5a122730d3cce87
mruniverse <yurilucaspg@gmail.com>
2023-08-17 18:17:19 -0300

    New test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
