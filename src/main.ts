b5ffc57db4825229c0b49a5087143a0c00553f04
mruniverse <yurilucaspg@gmail.com>
2023-08-17 18:28:16 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://dev.a3recovery.com.br',
+    origin: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     credentials: true,
   });
