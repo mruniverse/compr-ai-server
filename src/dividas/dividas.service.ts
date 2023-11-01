94309e61ebe6e288f905e91431e6fe030ee08712
mruniverse <yurilucaspg@gmail.com>
2023-11-01 17:32:36 -0300

    Fix license on divida

--- src/dividas/dividas.service.ts
+++ src/dividas/dividas.service.ts
@@ -2,7 +2,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateDividaDto } from './dto/create-divida.dto';
 import { UpdateDividaDto } from './dto/update-divida.dto';
-import { Prisma, Users } from '@prisma/client';
+import { Prisma, Users, Licenses } from '@prisma/client';
 
 @Injectable()
 export class DividasService {
@@ -84,7 +84,11 @@ export class DividasService {
     return this.prisma.dividas.create({
       data: {
         ...newDivida,
-        license_id,
+        Licenses: {
+          connect: {
+            id: license_id,
+          },
+        },
         StatusFaseDividas: {
           createMany: {
             data: fasesReguas.map((fase) => ({
