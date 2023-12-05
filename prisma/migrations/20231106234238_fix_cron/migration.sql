c9567a900227938084d4b96c94af39b5daf37157
Yuri Luz <yurilucaspg@gmail.com>
2023-12-05 15:25:45 -0300

    Remove migrations

--- prisma/migrations/20231106234238_fix_cron/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `dividas` ADD COLUMN `license_id` INTEGER NOT NULL DEFAULT 1;
-
--- AddForeignKey
-ALTER TABLE `Dividas` ADD CONSTRAINT `Dividas_license_id_fkey` FOREIGN KEY (`license_id`) REFERENCES `Licenses`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20231205172620_license_id_to_tipos_contrato/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `tiposcontrato` ADD COLUMN `licenca_id` INTEGER NOT NULL DEFAULT 1,
-    MODIFY `desconto_max` DOUBLE NULL;
-
--- AddForeignKey
-ALTER TABLE `TiposContrato` ADD CONSTRAINT `TiposContrato_licenca_id_fkey` FOREIGN KEY (`licenca_id`) REFERENCES `Licenses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
